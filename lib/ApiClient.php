a817796d6c136e6df80c20c8d135aa6616ef4d28
Victor Ndambakuwa <victor.ndambakuwa@wallee.com>
2020-08-10 10:56:23 +0200

    Release 2.0.5

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -54,7 +54,7 @@ final class ApiClient {
 	 *
 	 * @var string
 	 */
-	private $userAgent = 'PHP-Client/2.0.4/php';
+	private $userAgent = 'PHP-Client/2.0.5/php';
 
 	/**
 	 * The path to the certificate authority file.
